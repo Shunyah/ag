440993794ab645cfd89c4afc8a18ea9cf218ca64
Shunyah <63233042+Shunyah@users.noreply.github.com>
2020-04-13 13:54:32 +0300

    Update Main.cpp

--- Homework_4-12/Main.cpp
+++ Homework_4-12/Main.cpp
@@ -1,31 +1,28 @@
- // ConsoleApplication3.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
-#include <iostream>
 using namespace std;
-int dp(int n, int m) {
-    if (n <= 0 || m <= 0)
-        return 0;
-    if (n == 1 && m == 1)
-        return 1;
-    return (dp(n - 1, m - 2) + dp(n - 2, m - 1));
-}
+//    dp[a][b] += (dp[a - 1][b- 2] + dp[a - 2][b - 1]);
 int main()
 {
-    int n, int m;
+    int n;
+    int m;
+
+    int dp[n][m];
     cin >> n >> m;
-    cout << dp(n, m);
+    if (n <= 1 || m <= 1)
+        dp[n][m] = 0;
+
+    dp[2][1] = 1;
+    dp[1][2] = 1;
+
+    int i = 2;
+    int j = 2;
+
+    for (i < n; i++;) {
+        for (j < m; j++;) {
+            dp[n][m] += (dp[n - 1][m - 2] + dp[m - 2][n - 1]);
+        }
+    }
+    cout << dp[n][m] << endl;
+    system("pause");
     return 0;
 
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
