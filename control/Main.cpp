e457331d64fa34882a7860cea78ccff3c3335e15
Shunyah <63233042+Shunyah@users.noreply.github.com>
2020-04-27 11:57:17 +0300

    Update Main.cpp
    #include <iostream>
    using namespace std;
    
    int main()
    {
        int count = 0;
        int value[20] = {429,357,355,289,139,101,80,79,61,60,59,37,36,25,23,22,10,7,6,1};
        int sum;
        int i = 0;
        cin >> sum;
       while (sum > 0)
        {
            if (sum >= value[i])
            {
                sum = sum - value[i];
                count++;
            }
            else
                i++;
        }
        cout << count;
    }

--- control/Main.cpp
+++ control/Main.cpp
@@ -1,63 +1,22 @@
-// Example program
 #include <iostream>
-#include <vector>
 using namespace std;
-
-struct nn {
-    int l;
-    int r;
-}
-
-bool cmp(nn a, nn b) {
-    if (a.r > b.l) {
-        nn c = a;
-        a = b;
-        b = c;
-    }
-    if (a.r > b.l)
-        return true;
-    else
-        return false;
-}
 
 int main()
 {
-    int n;
-    vector <nn> les;
-    vector <vector <nn> > audit;
-
-    while (cin » n) {
-        for (int i = 0; i < n; ++i) {
-            nn c;
-            cin » c.l » c.r;
-            les.push_back(c)
+    int count = 0;
+    int value[20] = {429,357,355,289,139,101,80,79,61,60,59,37,36,25,23,22,10,7,6,1};
+    int sum;
+    int i = 0;
+    cin >> sum;
+   while (sum > 0)
+    {
+        if (sum >= value[i])
+        {
+            sum = sum - value[i];
+            count++;
         }
-        for (int i = 0; i < les.size(); ++i) {
-            int audit_num = -1;
-            for (int j = 0; j < (int)audit.size(); ++j) {
-                bool p = false;
-                for (int q = 0; q < audit[j].size(); ++q)
-                {
-                    if (cmp(audit[j][q], les[i]))
-                    {
-                        p = true;
-                        break;
-                    }
-                }
-                if (p == false) {
-                    audit_num = j;
-                    break;
-                }
-            }
-            if (audit_num == -1) {
-                vector<nn> audit_new(1, les[i]);
-                audit.push_back(audit_new);
-            }
-            else
-                audit[audit_num].push_back(les[i]);
-        }
-        cout << audit.size() << endl;
+        else
+            i++;
     }
-    system("pause");
-    return 0;
+    cout << count;
 }
