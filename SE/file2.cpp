fec29d6f9899730d76f60f9c2ecdbef90d88c37b
Shunyah <63233042+Shunyah@users.noreply.github.com>
2020-06-05 12:37:28 +0300

    Create file2.cpp

---
+++ SE/file2.cpp
@@ -0,0 +1,32 @@
+// Example program
+#include <iostream>
+using namespace std;
+class SetOfStacks {
+    int cap;
+   
+    stack< stack <int> > stack;
+    stack<int> st;;
+    
+     void push ( int v) {
+        stack last = getlaststack();
+        if (last != last.isFull())
+        last.push();
+        else 
+        Stack new  // create new stack
+    }
+     int рор // same with getting last stack
+     int getlaststack() {
+         if (stack.size !=0)
+         return stack.size()-1;
+         else reutrn NULL;
+         int popAt(int index) {
+             }
+             }
+             }
+
+
+int main(){
+    
+ retirn 0;
+}
+//at least i tried
